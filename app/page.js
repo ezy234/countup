0e2d77b6beb068397192147ea34e8ef091d47116
IsraelIyke <nwangwuisrael@gmail.com>
2024-03-24 19:32:37 +0100

    start tutorial

--- app/page.js
+++ app/page.js
@@ -1,66 +1,19 @@
 "use client";
 import { CountUp } from "countup.js";
 import { useEffect } from "react";
-import "animate.css";
-
 export default function Home() {
   useEffect(() => {
-    // const countUp = new CountUp("targetId", 5234, {
-    //   enableScrollSpy: true,
-    //   // scrollSpyOnce: true,
-    //   scrollSpyDelay: 3000,
-    // });
+    const countUp = new CountUp("countup", 5234);
 
-    new CountUp("targetId", 17, {
-      enableScrollSpy: true,
-      // scrollSpyOnce: true,
-      // scrollSpyDelay: 3000,
-    });
-
-    new CountUp("targetId2", 8401, {
-      enableScrollSpy: true,
-      // scrollSpyOnce: true,
-      // scrollSpyDelay: 3000,
-    });
-
-    new CountUp("targetId3", 234, {
-      enableScrollSpy: true,
-      // scrollSpyOnce: true,
-      // scrollSpyDelay: 3000,
-    });
-    // if (!countUp.error) {
-    //   countUp.start();
-    // } else {
-    //   console.error(countUp.error);
-    // }
+    if (!countUp.error) {
+      countUp.start();
+    } else {
+      console.error(countUp.error);
+    }
   }, []);
-
   return (
     <div>
-      <div className="banner">
-        <p className="banner-text1">Experience</p>
-        <p className="banner-text2 animate__animated animate__zoomInDown animate__delay-2s">
-          CountUp.js 🔼
-        </p>
-      </div>
-      <div className="count-down">
-        <p>We have great achievement to show</p>
-        <div>
-          <div style={{ display: "flex" }}>
-            <div id="targetId"></div>+
-          </div>
-          <p>yrs of experience</p>
-        </div>
-        <div>
-          <div id="targetId2"></div>
-          <p>successful projects</p>
-        </div>
-        <div>
-          <div id="targetId3"></div>
-          <p>clients</p>
-        </div>
-      </div>
-      <p className=" pt-[10rem]"></p>
+      <div id="countup"></div>
     </div>
   );
 }
