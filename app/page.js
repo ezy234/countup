2c551586d86b211fdd90e8aac8dbe069b444cbd2
IsraelIyke <nwangwuisrael@gmail.com>
2024-03-27 11:09:37 +0100

    start tutorial

--- app/page.js
+++ app/page.js
@@ -1,3 +1,20 @@
+"use client";
+import { CountUp } from "countup.js";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div></div>;
+  useEffect(() => {
+    const count = new CountUp("theTargetID", 590);
+
+    if (count.error) {
+      console.error(error);
+    } else {
+      count.start();
+    }
+  }, []);
+  return (
+    <div>
+      <div id="theTargetID"></div>
+    </div>
+  );
 }
